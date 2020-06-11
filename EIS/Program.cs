ff038a4ed9c3d8a6a8a35c7df7d1c1d5d9497045
Виталий.НПСО <vitaley.usa@gmail.com>
2020-06-11 21:42:54 +0500

    Add project files.

---
+++ EIS/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            X509Store store = new X509Store(StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadOnly);
+            X509Certificate2 certificate = null;
+            //manually chose the certificate in the store
+            X509Certificate2Collection sel = X509Certificate2UI.SelectFromCollection(store.Certificates, null, null, X509SelectionFlag.SingleSelection);
+
+            if (sel.Count > 0)
+            {
+                certificate = sel[0];
+                //System.Diagnostics.Process.Start("https://rosreestr.eisnot.ru/?auth_code=" + certificate.SerialNumber);
+                System.Diagnostics.Process.Start("https://rz.eisnot.ru/?auth_code=" + certificate.SerialNumber);
+            }
+        }
+    }
+}
